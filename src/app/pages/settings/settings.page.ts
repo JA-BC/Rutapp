ff6a1001324281e582bf749e540c2a38cf8b3c96
Jeferson A. Bernabel <jefersonbc000@gmail.com>
2022-05-30 21:57:32 -0400

    feat: Dark theme

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,10 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: 'settings.page.html'
 })
 export class SettingsPage implements OnInit {
-  constructor() { }
+
+  constructor(
+    @Inject(DOCUMENT) private readonly document: Document
+  ) { }
 
   ngOnInit() { }
+
+  onThemeChange() {
+    this.document.documentElement.classList.toggle('theme-dark');
+  }
 }
